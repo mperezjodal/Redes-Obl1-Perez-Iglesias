afdf1e5f3b14d390d8f5f99ccb1da0d6a00000c2
Marcelo Perez <marceloperez@Marcelos-MacBook-Air.local>
2021-09-14 18:25:50 -0300

    Code change

--- RedesObl1/ServerSocket/Display.cs
+++ RedesObl1/ServerSocket/Display.cs
@@ -9,24 +9,24 @@ namespace ServerSocket
         public static void ServerMenu() {
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine(@"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.ResetColor();
             Console.WriteLine();
             Console.WriteLine(@"        |¯\ /¯| | ____| |¯\ |¯| | | | |        ");
             Console.WriteLine(@"        |  ¯  | | __|   |  \| | | |_| |        ");
             Console.WriteLine(@"        |     | |_____| | \   | |_____|        ");
             Console.WriteLine();
-            Console.WriteLine(@"    Seleccione una opción:                     ");
+            Console.WriteLine("    Seleccione una opción:                     ");
             Console.WriteLine();
-            Console.WriteLine(@"    1-   Ver catálogo de juegos                ");
-            Console.WriteLine(@"    2-   Adquirir juego                        ");
-            Console.WriteLine(@"    3-   Publicar juego                        ");
-            Console.WriteLine(@"    4-   Publicar calificación de un juego     ");
-            Console.WriteLine(@"    5-   Buscar juegos                         ");
-            Console.WriteLine(@"         exit                                  ");
+            Console.WriteLine("    1-   Ver catálogo de juegos                ");
+            Console.WriteLine("    2-   Adquirir juego                        ");
+            Console.WriteLine("    3-   Publicar juego                        ");
+            Console.WriteLine("    4-   Publicar calificación de un juego     ");
+            Console.WriteLine("    5-   Buscar juegos                         ");
+            Console.WriteLine("         exit                                  ");
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine(@"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
             Console.ResetColor();
             Console.WriteLine();
         }
