ae5a1fb4099e54fc9e37223458de9b5bc46de24a
Marcelo Perez <marceloperez@Marcelos-MacBook-Air.local>
2021-09-29 17:11:35 -0300

    Removed comments

--- RedesObl1/FileStreamLibrary/FileCommunicationHandler.cs
+++ RedesObl1/FileStreamLibrary/FileCommunicationHandler.cs
@@ -24,32 +24,32 @@ namespace FileStreamLibrary
             byte[] fileNameData = Encoding.UTF8.GetBytes(fileName);
             int fileNameLength = fileNameData.Length;
             byte[] fileNameLengthData = BitConverter.GetBytes(fileNameLength);
-            // 1.- Envío el largo del nombre del archivo
+
             _socketStreamHandler.SendData(fileNameLengthData);
-            // 2.- Envío el nombre del archivo
+
             _socketStreamHandler.SendData(fileNameData);
 
             long fileSize = fileInfo.Length;
             byte[] fileSizeDataLength = BitConverter.GetBytes(fileSize);
-            // 3.- Envío el largo del archivo
+
             _socketStreamHandler.SendData(fileSizeDataLength);
-            // 4.- Envío los datos del archivo
+
             SendFile(fileSize, path);
         }
 
         public string ReceiveFile()
         {
-            // 1.- Recibir el largo del nombre del archivo
+
             byte[] fileNameLengthData = _socketStreamHandler.ReceiveData(Specification.FixedFileNameLength);
             int fileNameLength = BitConverter.ToInt32(fileNameLengthData);
-            // 2.- Recibir el nombre del archivo
+
             byte[] fileNameData = _socketStreamHandler.ReceiveData(fileNameLength);
             string fileName = Encoding.UTF8.GetString(fileNameData);
 
-            // 3.- Recibo el largo del archivo
+
             byte[] fileSizeDataLength = _socketStreamHandler.ReceiveData(Specification.FixedFileSizeLength);
             long fileSize = BitConverter.ToInt64(fileSizeDataLength);
-            // 4.- Recibo los datos del archivo
+
             ReceiveFile(fileSize, fileName);
             return fileName;
         }
