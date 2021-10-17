0b3125e4368a66b61f39c11e5be16835b236c44e
Federico Iglesias <71080777+figlesias221@users.noreply.github.com>
2021-10-17 17:19:35 -0300

    delete Socket naming

---
+++ RedesObl1/Client/Client.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using ProtocolLibrary;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using DisplayUtils;
+using networkStream;
+using Domain;
+
+namespace Client
+{
+    public class Client
+    {
+        public string ServerIpAddress  { get; set; }
+        public int ServerPort { get; set; }
+        public string ClientIpAddress  { get; set; }
+        public int ClientPort  { get; set; }
+
+        public static Dictionary<string, string> ClientMenuOptions = new Dictionary<string, string> {
+            {"1", "Publicar juego"},
+            {"2", "Modificar juego"},
+            {"3", "Eliminar juego"},
+            {"4", "Buscar juego"},
+            {"5", "Calificar juego"},
+            {"6", "Adquirir juego"},
+            {"7", "Ver juegos adquiridos"},
+            {"8", "Ver juegos y detalles"}
+        };
+
+        static void Main(string[] args)
+        {
+            string directory = Directory.GetCurrentDirectory();
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                    .SetBasePath(directory)
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+            var section = configuration.GetSection(nameof(Client));
+		    var ClientConfig = section.Get<Client>();
+
+            IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Parse(ClientConfig.ClientIpAddress), ClientConfig.ClientPort);
+            var tcpClient = new TcpClient(clientEndPoint);            
+            Console.WriteLine("Conectando al servidor...");
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(ClientConfig.ServerIpAddress), ClientConfig.ServerPort);
+            tcpClient.Connect(serverEndPoint);
+            Console.WriteLine("Conectado al servidor.");
+
+            RunApp(tcpClient);
+            tcpClient.Close();
+        }
+
+        public static void RunApp(TcpClient tcpClient)
+        {
+            var connected = true;
+
+            using (var networkStream = tcpClient.GetStream())
+            {
+                var headerRequestGameList = new Header(HeaderConstants.Request, CommandConstants.GetGames, 0);
+            
+                ClientUtils clientUtils = new ClientUtils(networkStream);
+                clientUtils.Login();
+                while (connected)
+                {
+                    var option = DialogUtils.Menu(ClientMenuOptions);
+
+                    switch (option)
+                    {
+                        case "exit":
+                            clientUtils.Logout();
+                            connected = false;
+                            break;
+                        case "1":
+                            clientUtils.PublishGame();
+                            break;
+                        case "2": 
+                            clientUtils.ModifyGame();
+                            break;
+                        case "3": 
+                            clientUtils.DeleteGame();
+                            break;
+                        case "4": 
+                            DialogUtils.SearchFilteredGames(clientUtils.GetGames());
+                            break;
+                        case "5":
+                            clientUtils.PublishReview();
+                            break;
+                        case "6": 
+                            clientUtils.AcquireGame();
+                            break;
+                        case "7": 
+                            clientUtils.ShowGamesAndDetail(clientUtils.GetAcquiredGames());
+                            break;
+                        case "8": 
+                            clientUtils.ShowGamesAndDetail(clientUtils.GetGames());
+                            break;
+                        default:
+                            Console.WriteLine("Opción inválida.");
+                            break;
+                    }
+                    DialogUtils.ReturnToMenu();
+                }
+            }
+        }
+    }
+}
---
+++ RedesObl1/Server/ServerMenuUtils.cs
@@ -0,0 +1,146 @@
+using System;
+using System.IO;
+using System.Linq;
+using DisplayUtils;
+using Domain;
+
+namespace Server
+{
+    public class ServerMenuUtils
+    {
+        private GameSystem gameSystem;
+        public ServerMenuUtils(GameSystem gs)
+        {
+            gameSystem = gs;
+        }
+
+        public void InsertGame()
+        {
+            Game gameToPublish = DialogUtils.InputGame();
+            try
+            {
+                if (gameToPublish.Cover != null)
+                {
+                    var fileName = gameToPublish.Cover.Split("/").Last();
+                    System.IO.File.Copy(gameToPublish.Cover, Directory.GetCurrentDirectory().ToString() + "/" + fileName);
+                    gameToPublish.Cover = fileName;
+                }
+
+                gameSystem.AddGame(gameToPublish);
+                Console.WriteLine("Se ha publicado el juego: " + gameToPublish.Title + ".");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+        }
+
+        public void InsertReview()
+        {
+            Game selectedGame = DialogUtils.SelectGame(gameSystem.Games);
+            if (selectedGame == null)
+            {
+                return;
+            }
+            if (gameSystem.IsGameBeingModified(selectedGame))
+            {
+                Console.WriteLine("No se puede publicar una califiación de este juego.");
+                return;
+            }
+
+            Review selectedGameReview = DialogUtils.InputReview();
+
+            if (selectedGameReview == null)
+            {
+                return;
+            }
+            if (gameSystem.IsGameBeingModified(selectedGame) || !gameSystem.GameExists(selectedGame))
+            {
+                Console.WriteLine("No se puede publicar una califiación de este juego.");
+                return;
+            }
+            try
+            {
+                selectedGame.AddReview(selectedGameReview);
+                Console.WriteLine("Se ha publicado la calificación del juego " + selectedGame.Title + ".");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void InsertUser()
+        {
+            try
+            {
+                User userToInsert = DialogUtils.InputUser(gameSystem.Users);
+                userToInsert.Login = false;
+                if (userToInsert == null)
+                {
+                    Console.WriteLine("No se puede insertar este usuario.");
+                    return;
+                }
+                else
+                {
+                    gameSystem.AddUser(userToInsert.Name);
+                    Console.WriteLine("Se ha insertado el usuario: " + userToInsert.Name + ".");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void ModifyUser()
+        {
+            try
+            {
+                User userToModify = DialogUtils.SelectUser(gameSystem.Users);
+
+                if (userToModify == null)
+                {
+                    Console.WriteLine("Retorno al menú.");
+                    return;
+                }
+
+                Console.WriteLine("Ingrese el nuevo nombre de usuario:");
+                User modifiedUser = DialogUtils.InputUser(gameSystem.Users);
+
+                if (modifiedUser == null)
+                {
+                    Console.WriteLine("Retorno al menú.");
+                    return;
+                }
+                gameSystem.UpdateUser(userToModify, modifiedUser);
+                Console.WriteLine("Se ha modificado el usuario: " + modifiedUser.Name + ".");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public void DeleteUser()
+        {
+            try
+            {
+                User userToDelete = DialogUtils.SelectUser(gameSystem.Users);
+                if (userToDelete == null)
+                {
+                    Console.WriteLine("Retorno al menú.");
+                    return;
+                }
+
+                gameSystem.Users.RemoveAll(u => u.Name.Equals(userToDelete.Name));
+                Console.WriteLine("Se ha eliminado el usuario: " + userToDelete.Name + ".");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
